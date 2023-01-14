40ce9f720dc2a3c3ce417d840a69fdd33109a5bc
Sean Tan <seantan1218@gmail.com>
2023-01-14 14:25:26 +0800

    Added form fields to vcf page

--- src/components/Layout/HomePage/Test.js
+++ src/components/Layout/HomePage/Test.js
@@ -1,8 +1,6 @@
 const Body = () => {
   return (
-    <div className="flex flex-col items-center mx-auto">
-      <h1 className="text-2xl">Woah</h1>
-      <p className="text-xl">Why arent you working</p>
+    <div>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
           <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
@@ -15,8 +13,6 @@ const Body = () => {
           </div>
         </div>
       </div>
-      <button class="btn btn-primary">Button</button>
-      <button class="btn w-64 rounded-full">Button</button>
     </div>
   );
 };
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 
 import Body from "../components/Layout/HomePage/Body";
+import Test from "../components/Layout/HomePage/Test";
 
 const HomePage = () => {
   return (
     <Fragment>
       <Body />
+      <Test />
     </Fragment>
   );
 };
