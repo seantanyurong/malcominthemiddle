aaf54b15345bccc91fb5f0a824d28bad8bcee6f5
Sean Tan <seantan1218@gmail.com>
2023-01-14 13:21:52 +0800

    Set up initial folder, file structure and added packages

---
+++ src/components/Layout/HomePage/Body.js
@@ -0,0 +1,12 @@
+const Body = () => {
+  return (
+    <>
+      <h1>Woah</h1>
+      <button class="btn">Button</button>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn w-64 rounded-full">Button</button>
+    </>
+  );
+};
+
+export default Body;
